9b17e5c6b5eecb05438619298a74c061b594c722
Gali Kertser <gali.kertser@gmail.com>
2026-08-30 14:42:14 +0300

    almost finished with the title-screen

--- app/src/components/Title.jsx
+++ app/src/components/Title.jsx
@@ -1,9 +1,26 @@
 import '../styles/Title.css'
 
 const Title = () => {
+
   return (
     <div className="Title">
-        <img id='credits-icon' src='/Title/Credits Icon.png'></img>
+
+      <div className="credits-icon">
+        <img
+          className="credits-icon-normal"
+          src="/Title/Credits Icon.png"
+        />
+
+        <img
+          className="credits-icon-open"
+          src="/Title/Credits Icon-Open.png"
+        />
+      </div>
+
+      <p className="title-text H2">
+        טכניקות פיקוד העורף על זירת אירוע
+      </p>
+
     </div>
   )
 }
